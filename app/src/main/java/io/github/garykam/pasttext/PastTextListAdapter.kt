98566c72e2082220db4dfefcfe6943695fae3290
GaryKam <garyhkam@gmail.com>
2021-04-13 13:14:59 -0700

    Show different icon for past texts that are unlocked

--- app/src/main/java/io/github/garykam/pasttext/PastTextListAdapter.kt
+++ app/src/main/java/io/github/garykam/pasttext/PastTextListAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -24,11 +25,18 @@ class PastTextListAdapter(
 
         holder.titleText.text = pastText.title
 
-        var timeDifference = pastText.unlockDate.time - Date().time
-        val days = timeDifference / (1000 * 60 * 60 * 24)
-        timeDifference %= 1000 * 60 * 60 * 24
-        val hours = timeDifference / (1000 * 60 * 60)
-        holder.timeText.text = context.getString(R.string.format_time, days, hours)
+        if (pastText.isUnlocked()) {
+            // Indicate that the Past Text is unlocked.
+            holder.timeImage.setImageResource(R.drawable.ic_lock_open)
+            holder.timeText.visibility = View.INVISIBLE
+        } else {
+            // Show the time remaining until the Past Text will unlock.
+            var timeDifference = pastText.unlockDate.time - Date().time
+            val days = timeDifference / (1000 * 60 * 60 * 24)
+            timeDifference %= 1000 * 60 * 60 * 24
+            val hours = timeDifference / (1000 * 60 * 60)
+            holder.timeText.text = context.getString(R.string.format_time, days, hours)
+        }
     }
 
     override fun getItemCount() = pastTexts.size
@@ -36,15 +44,19 @@ class PastTextListAdapter(
     inner class PastTextViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleText: TextView = itemView.findViewById(R.id.text_title)
         val timeText: TextView = itemView.findViewById(R.id.text_time)
+        val timeImage: ImageView = itemView.findViewById(R.id.image_time)
 
         init {
             itemView.setOnClickListener {
                 val pastText = pastTexts[adapterPosition]
+
                 if (pastText.isUnlocked()) {
+                    // Display details activity when the Past Text is clicked.
                     PastTextDetailsActivity.startActivity(
                         it.context, pastText.title, pastText.content
                     )
                 } else {
+                    // Alert the user that the Past Text is still locked.
                     MaterialAlertDialogBuilder(itemView.context)
                         .setMessage(R.string.locked_past_text)
                         .setPositiveButton(R.string.ok) { dialog, _ -> dialog.dismiss() }
